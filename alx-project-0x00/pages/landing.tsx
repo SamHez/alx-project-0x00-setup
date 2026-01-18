a803e77b1d7f03fda6810e69150ba448285c3fbd
Sam Epodoi <samuelepodoi@gmail.com>
2026-01-18 15:01:32 +0200

    4. Using components: Render multiple Card components in landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,16 @@
+import Card from "@/components/Card"
+import React from "react"
+
 const Landing: React.FC = () => {
     return (
         <div>
             <h1 className=" text-xl font-extralight">Landing Page</h1>
+            <div className="flex gap-4 p-4 flex-wrap">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
